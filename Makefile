58c46c5e6df807f96976349ac8768b6ab749854a
NikosMouzakitis <mzktsn@gmail.com>
2018-07-26 19:51:51 +0300

    Add files via upload
    Xor method , each character.

---
+++ Makefile
@@ -0,0 +1,8 @@
+all:
+	gcc main.c
+	./a.out 12345 input_file encrypted
+	./a.out 12345 encrypted decrypted
+	cat decrypted
+clean:
+	rm encrypted decrypted
+	
---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/*	Usage: ./a.out  key input_file output_file	*/
+
+int main( int argc, char *argv[])
+{
+	FILE *fi, *fo;
+	char *cp;
+
+	int c;
+
+	if ( (cp = argv[1]) && ( *cp != '\0') ) {
+
+		if( (fi = fopen(argv[2],"rb")) != NULL) {
+
+			if( (fo = fopen(argv[3], "wb")) != NULL) {
+
+				while( (c = getc(fi)) != EOF) {
+					if(!*cp)
+						cp = argv[1];
+					c ^=*(cp++);
+					putc(c,fo);		
+				}
+				fclose(fo);
+			}
+			fclose(fi);
+		}
+	}
+	return (0);
+}
